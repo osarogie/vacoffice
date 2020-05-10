6ab36a9480fe2317a7c5614825abe7b215941831
Emmanuel Nosakhare <nonreactiv@gmail.com>
2020-05-10 18:50:09 +0100

    Update gemfile and manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_directory ../javascripts .js
-//= link graphiql/rails/application.css
-//= link graphiql/rails/application.js
